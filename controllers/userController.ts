50e1995597c0e01ebe40a9db7e063066667ebba6
smt197 <serignembayet@gmail.com>
2024-08-17 21:14:33 +0000

    feature SMT Post release1

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -181,3 +181,53 @@ export const updateProfile = async (req: Request, res: Response) => {
     }
 };
 
+
+
+export const blockUser = async (req:Request, res:Response) => {
+    const blockerId = (req as any).userId; 
+    const blockedId = parseInt(req.params.blockedId); 
+
+    try {
+        if (!blockerId) {
+            return res.status(401).json({ message: 'You are not authorized to block this user.' });
+        }
+
+        if (blockerId === blockedId) {
+            return res.status(400).json({ message: 'You cannot block yourself.' });
+        }
+
+        const blockedUser = await prisma.user.findUnique({
+            where: { id: blockedId },
+        });
+
+        if (!blockedUser) {
+            return res.status(404).json({ message: `User with ID ${blockedId} not found` });
+        }
+
+        const existingBlock = await prisma.block.findUnique({
+            where: {
+                blockerId_blockedId: {
+                    blockerId: blockerId,
+                    blockedId: blockedId,
+                },
+            },
+        });
+
+        if (existingBlock) {
+            return res.status(400).json({ message: 'This user is already blocked.' });
+        }
+
+        // Créer un nouveau blocage
+        const block = await prisma.block.create({
+            data: {
+                blockerId: blockerId,
+                blockedId: blockedId,
+            },
+        });
+
+        res.status(200).json({ message: 'User blocked successfully', block });
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to block user', error });
+    }
+};
+
