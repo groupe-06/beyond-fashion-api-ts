0e5f0df7aee3c69246205cb8ef80858a6acba10c
diallo ibrahima kaba <dialloibhazard@gmail.com>
2024-10-15 17:45:08 +0000

    method not found

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -332,13 +332,14 @@ export const verifyValidityToken = async (req: Request, res: Response) => {
 
     // Extraire le token (enlever 'Bearer ')
     const token = authHeader.split(' ')[1];
+    console.log('Token:', token);
 
     // Vérifier si Prisma est bien connecté et si le modèle est disponible
     console.log('Prisma blackListToken model:', prisma.blackListToken);
 
     try {
         // Vérifier si le token est blacklisté
-        const blacklistedToken = await prisma.blackListToken.findUnique({
+        const blacklistedToken = await prisma.blackListToken.findUniqueOrThrow({
             where: {
                 token: token
             }
