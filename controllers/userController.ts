ad67ba44f58290969aea4fb877a667634aa2b59f
diallo ibrahima kaba <dialloibhazard@gmail.com>
2024-10-15 16:52:54 +0000

    mise à jour de la fonction verify token

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -322,29 +322,40 @@ export const unblockUser = async (req: Request, res: Response) => {
 };
 
 export const verifyValidityToken = async (req: Request, res: Response) => {
-    const {token} = req.body;
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorized: No token provided' });
+    // Récupérer le header Authorization
+    const authHeader = req.headers.authorization;
+
+    // Vérifier si le token est présent dans le header et qu'il commence bien par 'Bearer'
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Unauthorized: No token provided or incorrect format' });
     }
 
-    try{
+    // Extraire le token (enlever 'Bearer ')
+    const token = authHeader.split(' ')[1];
+
+    try {
+        // Vérifier si le token est blacklisté
         const blacklistedToken = await prisma.blackListToken.findUnique({
             where: {
                 token: token
             }
         });
-    
-        if(blacklistedToken) {
+
+        if (blacklistedToken) {
             return res.status(401).json({ message: 'Unauthorized: Token blacklisted' });
         }
 
+        // Décoder et vérifier le token JWT
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET || '9f86d081884c7d659a2feaa0c55ad023') as jwt.JwtPayload;
-        return res.status(200).json({ message: 'Valid token'});
-    }catch(error) {
+        console.log(decodedToken);
+
+        return res.status(200).json({ message: 'Valid token' });
+    } catch (error) {
         console.log('Token verification error:', error);
         return res.status(401).json({ message: 'Unauthorized: Invalid or expired token' });
     }
-}
+};
+
 
 //res.clearCookie('token');
 //return res.status(200).json({ message: 'Logged out successfully' });
