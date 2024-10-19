6d75a70167b1bf21c4fefaec260b1d04503b2757
diallo ibrahima kaba <dialloibhazard@gmail.com>
2024-10-19 22:39:42 +0000

    modification de notification controller

--- controllers/notificationController.ts
+++ controllers/notificationController.ts
@@ -1,13 +1,18 @@
 import prisma from '../database/db.config';
 
-async function sendNotification(receiverId: number, emetorId = null, content: string, type: string, postId = null) {
-    if(emetorId){
-        
-    }
+async function sendNotification(receiverId: number, emetorId: number | null = null, content: string, type: string, postId: number | null = null) {
     await prisma.notification.create({
         data: {
-            receiverId: receiverId,
-            emetorId: emetorId,
+            receiver: {
+                connect: {
+                    id: receiverId
+                }
+            },
+            emetor: emetorId ? {
+                connect: {
+                    id: emetorId
+                }
+            } : undefined,
             content: content,
             type: type,
             postId: postId
