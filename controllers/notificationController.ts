e0fdba3f32bc9346b9153927eb7b3436d61e9c8c
diallo ibrahima kaba <dialloibhazard@gmail.com>
2024-10-19 23:04:45 +0000

    toujours dans

--- controllers/notificationController.ts
+++ controllers/notificationController.ts
@@ -3,19 +3,11 @@ import prisma from '../database/db.config';
 async function sendNotification(receiverId: number, emetorId: number | null = null, content: string, type: string, postId: number | null = null) {
     await prisma.notification.create({
         data: {
-            receiver: {
-                connect: {
-                    id: receiverId
-                }
-            },
-            emetor: emetorId ? {
-                connect: {
-                    id: emetorId
-                }
-            } : undefined,
-            content: content,
-            type: type,
-            postId: postId
+            receiverId,
+            emetorId,
+            content,
+            type,
+            postId
         },
     });
 }
---
+++ prisma/migrations/20241019230356_fix_notification_relations/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "notification_receiverId_idx" ON "notification"("receiverId");
+
+-- CreateIndex
+CREATE INDEX "notification_emetorId_idx" ON "notification"("emetorId");
