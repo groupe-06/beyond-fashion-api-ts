8763b969afc14b845243e54b647f500773cb5cac
babaissandiaye20 <babaissandiaye31@gmail.com>
2024-10-20 20:47:24 +0000

    messages

--- routes/messageRoute.ts
+++ routes/messageRoute.ts
@@ -1,11 +1,12 @@
 import {Router} from "express";
-import { createMessage, getMessages } from "../controllers/messageController";
+import { createMessage, getMessages,deleteMessage } from "../controllers/messageController";
 import { getToken } from "../middlewares/authMiddlewares";
 
 const router = Router();
 
 router.post("/new", getToken, createMessage);
 router.get("/connected-user", getToken, getMessages);
-router.delete("/delete/:id", getToken, getMessages);
+router.delete("/delete/:id", getToken, deleteMessage);
+
 
 export default router;
