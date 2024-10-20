0c791d80d52fd6dfdcdacfd0083a5c04c3b85b3c
groupe-06 <groupe6p6@gmail.com>
2024-10-20 14:32:19 +0000

    bd

--- database/db.config.ts
+++ database/db.config.ts
@@ -1,7 +1,7 @@
 import {PrismaClient} from "@prisma/client";
 
 const prisma: PrismaClient = new PrismaClient({
-    //log: ['query', 'error']
+    log: ['query', 'error']
 });
 
 export default prisma;
