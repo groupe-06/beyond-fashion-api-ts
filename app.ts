581c1097d9dd9545aae210b97fcb467fdc7fd56a
smt197 <105449441+smt197@users.noreply.github.com>
2025-11-19 17:25:42 +0000

    update url

--- app.ts
+++ app.ts
@@ -28,7 +28,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:8000", "http://localhost:3000", "https://beyond-fashion-api-ts-8ruc.onrender.com"],
+    origin: ["http://localhost:8000", "http://localhost:3000", "https://beyond-fashion-api-ts-8ruc.onrender.com", "https://threadline-front.onrender.com"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Origin, X-Requested-With, Content, Accept, Content-Type, Authorization']
 }));
